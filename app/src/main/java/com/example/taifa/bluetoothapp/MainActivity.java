517de2a3c591bb4b9771f9d17664c89b47963d55
Elvisthacoder <ekk213@gmail.com>
2016-03-22 16:27:34 +0300

    extending Main activity class

--- app/src/main/java/com/example/taifa/bluetoothapp/MainActivity.java
+++ app/src/main/java/com/example/taifa/bluetoothapp/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.taifa.bluetoothapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCbt = (Button) findViewById(R.id.btn_cbt_activity);
+        Button btnBle = (Button) findViewById(R.id.btn_ble_activity);
+
+        btnCbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ClassicBluetoothActivity.class));
+            }
+        });
+
+        btnBle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BleActivity.class));
+            }
+        });
     }
 }
+
