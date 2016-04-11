a8f590c807a1396a66e1b776d269adcb58c1fd75
Elvisthacoder <ekk213@gmail.com>
2016-04-11 18:43:13 +0300

    private

--- app/src/main/java/com/example/taifa/bluetoothapp/BleActivity.java
+++ app/src/main/java/com/example/taifa/bluetoothapp/BleActivity.java
@@ -1,7 +1,26 @@
 package com.example.taifa.bluetoothapp;
 
+import android.support.v7.app.AppCompatActivity;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by taifa on 4/11/16.
  */
-public class BleActivity {
+public class BleActivity extends AppCompatActivity {
+
+    private BluetoothLEController mBLEController;
+
+    private List<String> mList;
+    private BaseAdapter mFoundAdapter;
+
+    private ListView lvDevices;
+    private Button btnScan, btnDisconnect, btnReconnect, btnSend;
+    private TextView tvConnState, tvContent;
+    private EditText etSendContent;
 }
