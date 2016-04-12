4c810af85ea7ab58f216d8c68e378680fc7ed4f7
Elvisthacoder <ekk213@gmail.com>
2016-04-12 13:14:15 +0300

    chat class

---
+++ app/src/main/java/com/example/taifa/bluetoothapp/ChatActivity.java
@@ -0,0 +1,22 @@
+package com.example.taifa.bluetoothapp;
+
+/**
+ * Created by taifa on 4/12/16.
+ */
+
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import co.lujun.lmbluetoothsdk.BluetoothController;
+import co.lujun.lmbluetoothsdk.base.BluetoothListener;
+import co.lujun.lmbluetoothsdk.base.State;
+
+public class ChatActivity {
+}
