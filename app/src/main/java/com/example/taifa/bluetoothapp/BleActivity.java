e9c5679451f1d70e52a339815a8dc6672f32b5d1
Elvisthacoder <ekk213@gmail.com>
2016-04-12 11:38:59 +0300

    length

--- app/src/main/java/com/example/taifa/bluetoothapp/BleActivity.java
+++ app/src/main/java/com/example/taifa/bluetoothapp/BleActivity.java
@@ -188,6 +188,35 @@ public class BleActivity extends AppCompatActivity {
         mBLEController.release();
     }
 
+    private String parseData(BluetoothGattCharacteristic characteristic) {
+        String result = "";
+        // This is special handling for the Heart Rate Measurement profile.  Data parsing is
+        // carried out as per profile specifications:
+        // http://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
+//        if (UUID_HEART_RATE_MEASUREMENT.equals(characteristic.getUuid())) {
+//            int flag = characteristic.getProperties();
+//            int format = -1;
+//            if ((flag & 0x01) != 0) {
+//                format = BluetoothGattCharacteristic.FORMAT_UINT16;
+//                Log.d(TAG, "Heart rate format UINT16.");
+//            } else {
+//                format = BluetoothGattCharacteristic.FORMAT_UINT8;
+//                Log.d(TAG, "Heart rate format UINT8.");
+//            }
+//            final int heartRate = characteristic.getIntValue(format, 1);
+//            Log.d(TAG, String.format("Received heart rate: %d", heartRate));
+//            intent.putExtra(EXTRA_DATA, String.valueOf(heartRate));
+//        } else {
+        // For all other profiles, writes the data formatted in HEX.
+        final byte[] data = characteristic.getValue();
+        if (data != null && data.length > 0) {
+            result = new String(data);
+        }
+//        }
+        return result;
+    }
+}
+
 
 
 
