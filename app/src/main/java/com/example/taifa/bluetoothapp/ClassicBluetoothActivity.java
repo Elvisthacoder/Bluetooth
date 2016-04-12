7c7b017e5518159bbe9f6934cb5ff0aaf31b31ef
Elvisthacoder <ekk213@gmail.com>
2016-04-12 15:33:21 +0300

    listener

--- app/src/main/java/com/example/taifa/bluetoothapp/ClassicBluetoothActivity.java
+++ app/src/main/java/com/example/taifa/bluetoothapp/ClassicBluetoothActivity.java
@@ -1,7 +1,36 @@
 package com.example.taifa.bluetoothapp;
 
+import android.support.v7.app.AppCompatActivity;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by taifa on 4/12/16.
  */
 //public class ClassicBluetoothActivity {
 //}
+
+public class ClassicBluetoothActivity extends AppCompatActivity {
+
+    private BluetoothController mBluetoothController;
+
+    private Button btnScanAvaliabe, btnScan, btnOpen, btnStartServer;
+    private TextView tvBTState;
+    private ListView lvDevices;
+
+    private List<String> mList;
+    private BaseAdapter mFoundAdapter;
+    private String mMacAddress;
+
+    private static final String TAG = "LMBluetoothSdk";
+
+    private BluetoothListener mListener = new BluetoothListener() {
+        @Override
+        public void onActionStateChanged(int preState, int state) {
+            tvBTState.setText("Bluetooth state: " + Utils.transBtStateAsString(state));
+        }
+
